5984d030a69378fe592e65c3bb54bb421796d9eb
SeH <seh1seh@yandex.com>
2018-07-01 20:14:20 -0400

    updates

--- lab/src/main/java/nars/experiment/Gradius.java
+++ lab/src/main/java/nars/experiment/Gradius.java
@@ -77,15 +77,20 @@ public class Gradius extends NAgentX {
 //                (b) -> g.paused = b);
 
         actionUnipolar($.prop(this.nar.self(), $.the("deep")), (d)->{
-            //deep incrases both duration and max term volume
-            this.nar.time.dur(Util.lerp(d*d, 20, 120));
-            this.nar.termVolumeMax.set(Util.lerp(d, 30, 50));
+            if (d == d) {
+                //deep incrases both duration and max term volume
+                this.nar.time.dur(Util.lerp(d * d, 20, 120));
+                this.nar.termVolumeMax.set(Util.lerp(d, 30, 80));
+            }
             return d;
         });
 
         actionUnipolar($.prop(this.nar.self(), $.the("awake")), (a)->{
-            this.nar.activateConceptRate.set(Util.lerp(a, 0.2f, 1f));
+            if (a == a) {
+                this.nar.activateConceptRate.set(Util.lerp(a, 0.2f, 1f));
+            }
             return a;
+
         });
 //        actionUnipolar($.prop(nar.self(), $.the("focus")), (a)->{
 //            nar.forgetRate.set(Util.lerp(a, 0.9f, 0.8f)); //inverse forget rate
