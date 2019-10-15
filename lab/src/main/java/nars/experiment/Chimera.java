3038ebfbb5312895db47e9e3b7eeb6862feb7a17
Jim Northrup <jim@vsiwest.com>
2019-10-16 01:02:11 +0700

    two 2d planar games running simultaneously

---
+++ lab/src/main/java/nars/experiment/Chimera.java
@@ -0,0 +1,25 @@
+package nars.experiment;
+
+import nars.GameX;
+import nars.gui.sensor.VectorSensorChart;
+
+import static nars.experiment.Tetris.*;
+import static spacegraph.SpaceGraph.window;
+
+public
+class Chimera{
+
+    public static void main(String... args) {  GameX.runRT(n -> {
+
+            var t = new Tetris(n, tetris_width, tetris_height);
+            var g = new Gradius(n);
+            n.add(t);
+
+            window(new VectorSensorChart(t.gridVision, t).withControls(), 400, 800);
+
+            n.add(g);
+            return g;
+        }, FPS * thinkPerFrame);
+
+    }
+}
