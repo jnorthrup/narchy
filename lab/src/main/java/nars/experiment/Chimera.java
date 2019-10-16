8a398ed6f936a9138e363b056b55f49d4ad7df48
Jim Northrup <jim@vsiwest.com>
2019-10-16 14:00:55 +0700

    chimera avg err off

--- lab/src/main/java/nars/experiment/Chimera.java
+++ lab/src/main/java/nars/experiment/Chimera.java
@@ -7,23 +7,27 @@ import static nars.experiment.Tetris.*;
 import static spacegraph.SpaceGraph.window;
 
 public
-class Chimera{
+class Chimera {
 
-    public static void main(String... args) {  GameX.runRT(n -> {
-
-
-System.setProperty("tetris.can.fall","true");
-System.setProperty("tetris.fall.rate","5");
-        System.setProperty("tetris.use.density","false");
-        System.setProperty("tetris.easy","true");
+    public static void main(String... args) {
+        GameX.runRT(n -> {
+            //potential boredom without the down button.
+            System.setProperty("tetris.fall.rate", "5");
+            System.setProperty("tetris.can.fall", "true");
+            //survival only.
+            System.setProperty("tetris.use.density", "false");
+            //dot
+            System.setProperty("tetris.easy", "true");
+//            reduce con io
+            System.setProperty("avg.err", "false");
 
             var t = new Tetris(n, tetris_width, tetris_height);
             var g = new Gradius(n);
 
 
-        var x = new NARio(n);
-        n.add(x);
-        n.add(t);
+            var x = new NARio(n);
+            n.add(x);
+            n.add(t);
 
             window(new VectorSensorChart(t.gridVision, t).withControls(), 400, 800);
             n.add(g);
